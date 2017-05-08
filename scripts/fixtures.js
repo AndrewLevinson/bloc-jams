0765641d670c72eec618bed646c9255a1dbeb9be
andrew_l <andrewlevins@gmail.com>
2017-05-08 09:24:22 -0500

    checkpoint 31 complete

---
+++ scripts/fixtures.js
@@ -0,0 +1,95 @@
+// Example Album
+var albumPicasso = {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        {
+            title: 'Blue',
+            duration: '4:26'
+        },
+        {
+            title: 'Green',
+            duration: '3:14'
+        },
+        {
+            title: 'Red',
+            duration: '5:01'
+        },
+        {
+            title: 'Pink',
+            duration: '3:21'
+        },
+        {
+            title: 'Magenta',
+            duration: '2:15'
+        }
+     ]
+};
+
+// Another Example Album
+var albumMarconi = {
+    title: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        {
+            title: 'Hello, Operator?',
+            duration: '1:01'
+        },
+        {
+            title: 'Ring, ring, ring',
+            duration: '5:01'
+        },
+        {
+            title: 'Fits in your pocket',
+            duration: '3:21'
+        },
+        {
+            title: 'Can you hear me now?',
+            duration: '3:14'
+        },
+        {
+            title: 'Wrong phone number',
+            duration: '2:15'
+        }
+     ]
+};
+
+
+// Third Example Album
+var albumB = {
+    title: 'Licensed to Ill',
+    artist: 'Beastie Boys',
+    label: 'Def Jam',
+    year: '1986',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        {
+            title: 'Rhymin & Stealin',
+            duration: '4:08'
+        },
+        {
+            title: 'The New Style',
+            duration: '4:35'
+        },
+        {
+            title: 'She\'s Crafty',
+            duration: '3:35'
+        },
+        {
+            title: 'Posse in Effect',
+            duration: '2:26'
+        },
+        {
+            title: 'Slow Ride',
+            duration: '2:57'
+        }
+     ]
+};
+
+
